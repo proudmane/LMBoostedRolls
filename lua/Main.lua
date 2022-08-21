27d0c79e02fc7afd5d1feba328083a8fcee88330
proudmane <proudmane1@gmail.com>
2022-08-21 12:53:33 -0400

    event handling

--- lua/Main.lua
+++ lua/Main.lua
@@ -15,20 +15,20 @@ local defaults = {
 
 function Main:OnInitialize()
     Main.db = LibStub("AceDB-3.0"):New("LMBoostedRollsDB", defaults)
-    
-    Main:RegisterEvent(
-        'CHAT_MSG_LOOT',
-        onChatMsgLoot
-    )
 
-    Main:HelloWorld()
+    -- Main:HelloWorld()
+
+    message("test")
+
+    local frame = CreateFrame("Frame")
+    frame:RegisterEvent("CHAT_MSG_SYSTEM")
+    frame:SetScript("OnEvent", function (test2, event, test)
+        local test = string.find(event, "[abc]")
+        print(test)
+    end)
 end
 
-local function onChatMsgLoot(arg1, arg2)
-    message("testing")
-end
-
-function Main:HelloWorld()
-    message(Main.CSV:Test())
-end
+-- function Main:HelloWorld()
+--     message(Main.CSV:Test())
+-- end
 
